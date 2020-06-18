880a1b604da2eaf99b14d2b3adf7f2d5d8ee7913
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-06-18 14:24:19 +1000

    feat(gh-pages): build

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,5 @@ import { MyApp } from './my-app';
 
 Aurelia
   .register(RouterConfiguration)
-  // To use HTML5 pushState routes, replace previous line with the following
-  // customized router config.
-  // .register(RouterConfiguration.customize({ useUrlFragmentHash: false }))
   .app(MyApp)
   .start();
