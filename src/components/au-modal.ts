898ff98d9c64071f679907d915675ee9bc4f2606
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-06-18 11:07:53 +1000

    feat(modal): added modal element

---
+++ src/components/au-modal.ts
@@ -0,0 +1,71 @@
+import { bindable, ICustomElementViewModel, customElement } from '@aurelia/runtime';
+import { shadowCSS } from '@aurelia/runtime-html';
+
+import SharedStyles from '../variables.css';
+
+import styles from './au-modal.css';
+import template from './au-modal.html';
+
+export type ModalSize = 'sm' | 'md' | 'lg' | number;
+export type ModalRole = 'alertdialog' | 'dialog';
+
+const SIZES = {
+    sm: '540px',
+    md: '720px',
+    lg: '900px'
+};
+
+const ESCAPE_KEY_CODE = 27;
+
+@customElement({
+    name: 'au-modal',
+    template,
+    dependencies: [
+        shadowCSS(SharedStyles, styles)
+    ],
+    shadowOptions: { mode: 'open' }
+})
+export class AuModalCustomElement implements ICustomElementViewModel<Element> {
+    @bindable private size: ModalSize = 'sm';
+    @bindable private label: string = '';
+    @bindable private clickClose: boolean = true;
+    @bindable private onClose = (event?: MouseEvent) => ``;
+    @bindable private heading: string = '';
+    @bindable private role: ModalRole = 'dialog';
+
+    private width;
+
+    beforeAttach() {
+        this.width = typeof SIZES[this.size] !== 'undefined' ? SIZES[this.size] : this.size;
+
+        window.addEventListener('keyup', this.handleKeyUp);
+    }
+
+    afterAttach() {
+
+    }
+
+    beforeDetach() {
+        window.removeEventListener('keyup', this.handleKeyUp);
+    }
+
+    sizeChanged(size) {
+        this.width = typeof SIZES[size] !== 'undefined' ? SIZES[size] : size;
+    }
+
+    handleKeyUp = (event: { keyCode: number }) => {
+        if (event.keyCode === ESCAPE_KEY_CODE) {
+            this.onClose();
+        }
+    }
+
+    handleBackdropClick(event: MouseEvent) {
+        if (this.clickClose) {
+            if (event.target !== event.currentTarget) {
+                return;
+            }
+    
+            this.onClose(event);
+        }
+    }
+}
--- src/configuration.ts
+++ src/configuration.ts
@@ -1,10 +1,12 @@
+import { AuModalCustomElement } from './components/au-modal';
 import { AuImageCustomElement } from './components/au-image';
 import { AuButtonCustomElement } from './components/au-button';
 import { DI, IContainer, IRegistry } from '@aurelia/kernel';
 
 export const DefaultComponents: IRegistry[] = [
     AuButtonCustomElement as unknown as IRegistry,
-    AuImageCustomElement as unknown as IRegistry
+    AuImageCustomElement as unknown as IRegistry,
+    AuModalCustomElement as unknown as IRegistry
 ];
 
 const cardiganConfiguration = {
