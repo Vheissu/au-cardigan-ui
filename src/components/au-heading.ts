a03b284d004e03d6a4f2e4c2b2842d6083e123e9
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-06-25 11:11:02 +1000

    feat(heading): component for headings

---
+++ src/components/au-heading.ts
@@ -0,0 +1,22 @@
+import { bindable, ICustomElementViewModel, customElement, BindingMode } from '@aurelia/runtime';
+import { shadowCSS, cssModules } from '@aurelia/runtime-html';
+
+import SharedStyles from '../variables.css';
+
+import styles from './au-heading.css';
+import template from './au-heading.html';
+@customElement({
+    name: 'au-heading',
+    template,
+    dependencies: [
+        shadowCSS(SharedStyles, styles)
+    ],
+    shadowOptions: { mode: 'open' }
+})
+export class AuHeadingCustomElement implements ICustomElementViewModel<Element> {
+    @bindable public size: 'small' | 'medium' | 'large' = 'medium';
+    @bindable public level: '1' | '2' | '3' | '4' | '5' | '6' = '1';
+    @bindable public color: 'white' | 'light' | 'dark' | 'primary' | 'success' | 'info' | 'error' | 'bright' | 'skyBlue' | 'purple' | 'blueAlt' = 'dark';
+    @bindable public overflow: 'normal' | 'breakWord' = 'normal';
+    @bindable public truncate = false;
+}
