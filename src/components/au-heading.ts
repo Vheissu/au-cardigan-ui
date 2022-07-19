12ec7685b1865439ce9d2ff8dab922713cfc2107
Dwayne <dwaynecharrington@gmail.com>
2022-07-19 21:56:24 +1000

    feat(components): validation for heading levels to fallback to default

--- src/components/au-heading.ts
+++ src/components/au-heading.ts
@@ -4,6 +4,10 @@ import SharedStyles from '../variables.css';
 
 import styles from './au-heading.css';
 import template from './au-heading.html';
+
+// Create a constant and populate it with an array of string numbers from 1 to 6
+const headingLevels = Array.from(Array(6).keys()).map(i => `${i + 1}`);
+
 @customElement({
     name: 'au-heading',
     template,
@@ -14,7 +18,15 @@ import template from './au-heading.html';
 })
 export class AuHeadingCustomElement implements ICustomElementViewModel {
     @bindable public size: 'small' | 'medium' | 'large' = 'medium';
-    @bindable public level: '1' | '2' | '3' | '4' | '5' | '6' = '1';
+    @bindable({
+        set: val => {
+            if (headingLevels.includes(val?.toString())) {
+                return val;
+            } else {
+                return '1';
+            }
+        }
+    }) public level: '1' | '2' | '3' | '4' | '5' | '6' = '1';
     @bindable public color: 'white' | 'light' | 'dark' | 'primary' | 'success' | 'info' | 'error' | 'bright' | 'skyBlue' | 'purple' | 'blueAlt' = 'dark';
     @bindable public overflow: 'normal' | 'breakWord' = 'normal';
     @bindable public truncate = false;
