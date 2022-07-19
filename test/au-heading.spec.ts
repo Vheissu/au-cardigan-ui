06644a83364dbbf69850e044fe74a46416df9276
Dwayne <dwaynecharrington@gmail.com>
2022-07-19 15:20:34 +1000

    test(components): heading component

---
+++ test/au-heading.spec.ts
@@ -0,0 +1,21 @@
+import { AuHeadingCustomElement } from './../src/components/au-heading';
+import { createFixture } from '@aurelia/testing';
+import { useShadowDOM, ICustomElementViewModel } from '@aurelia/runtime-html';
+
+const App = class App {
+    size = 'medium';
+};
+
+describe('Heading', () => {
+
+    test('should render heading element', async () => {
+        const { appHost, startPromise, tearDown } = createFixture('<au-heading>My Heading</au-heading>', App, [ AuHeadingCustomElement ]);
+
+        await startPromise;
+
+        expect(appHost.innerHTML).toContain('<h1>');
+
+        await tearDown;
+    });
+
+});
