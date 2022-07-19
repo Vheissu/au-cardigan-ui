f19e256fceba5f1d14e7f9ea22583cb4f7d8da20
Dwayne <dwaynecharrington@gmail.com>
2022-07-19 21:55:51 +1000

    test(helpers): reimplementing fixture creation

---
+++ test/helpers.ts
@@ -0,0 +1,31 @@
+import { TestContext } from '@aurelia/testing';
+import { Aurelia, CustomElement, StyleConfiguration } from '@aurelia/runtime-html';
+
+export async function bootstrap(template, $class, dependencies: any[] = []) {
+    const ctx = TestContext.create();
+    const container = ctx.container;
+    const platform = ctx.platform;
+
+    const root = ctx.doc.body.appendChild(ctx.createElement('div'));
+    const host = root.appendChild(ctx.createElement('app'));
+
+    container.register(...dependencies);
+
+    const au = new Aurelia(container);
+
+    const App = CustomElement.define({
+        name: 'app',
+        template,
+    }, $class);
+
+    const component = container.get(App);
+
+    au.app({ component, host });
+
+    await au.start();
+
+    return { container, ctx, au, host, platform, destroy: async () => {
+        await au.stop();
+        host.remove();
+    } };
+}
